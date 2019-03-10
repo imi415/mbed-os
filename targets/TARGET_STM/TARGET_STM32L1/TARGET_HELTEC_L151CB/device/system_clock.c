72e9214f75d1d1fc505bc26a4999ba2ed90893ca
imi415 <imi415.public@gmail.com>
2019-03-10 21:48:31 +0800

    Use HSI.

--- targets/TARGET_STM/TARGET_STM32L1/TARGET_HELTEC_L151CB/device/system_clock.c
+++ targets/TARGET_STM/TARGET_STM32L1/TARGET_HELTEC_L151CB/device/system_clock.c
@@ -47,7 +47,7 @@
 
 /* Select the clock sources (other than HSI) to start with (0=OFF, 1=ON) */
 #define USE_PLL_HSE_EXTC (0) /* Use external clock */
-#define USE_PLL_HSE_XTAL (1) /* Use external xtal */
+#define USE_PLL_HSE_XTAL (0) /* Use external xtal */
 
 
 #if (USE_PLL_HSE_XTAL != 0) || (USE_PLL_HSE_EXTC != 0)
